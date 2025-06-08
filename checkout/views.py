75ec3b3cdd5d54aee96d7eeb8e2f22dac32a6952
Chris Elliott <acelliott1218@gmail.com>
2025-06-08 10:09:08 +0000

    begin stripe integration

--- checkout/views.py
+++ checkout/views.py
@@ -12,6 +12,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51RXfurPEDyt24C2McypyH2xXMQwELvdsMjWwvxRZTEKm4hiiJ6nE1uY9iNPKc5AVYXddE1d0eJ5lIMgf0FADE3yN00NEaPFPzU',
+        'client_secret': 'test client secret'
     }
 
     return render(request, template, context)
